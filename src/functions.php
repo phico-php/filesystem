8fbf2b7b2e5bbab82438eb2290888605080edae8
indgy <65541311+indgy@users.noreply.github.com>
2024-06-24 11:30:20 +0100

    Added path() function

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+// path cannot be overridden
+function path(string $str = ''): string
+{
+    $root = str_replace('/public', '', getcwd());
+    // return sprintf('%s/%s', str_replace('/src', '', __DIR__), ltrim($str, '/'));
+    $str = str_replace(['\\', '/./', '/../'], '/', trim($str));
+    $str = preg_replace('|[^a-z0-9\*\-\_\./]|i', '', $str);
+    $path = sprintf('%s/%s', $root, ltrim($str, '/'));
+
+    return $path;
+}
+
 if (!function_exists('files')) {
     function files(): \Phico\Filesystem\Files
     {
