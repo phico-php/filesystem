943e53313323688eada7d6d1197f7a7812cd7001
indgy <65541311+indgy@users.noreply.github.com>
2024-09-02 10:05:23 +0100

    path() function must rely on accurate root folder, require PHICO_PATH_ROOT definition

--- src/functions.php
+++ src/functions.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 // path cannot be overridden
 function path(string $str = ''): string
 {
-    $root = str_replace('/public', '', getcwd());
+    //    $root = str_replace('/public', '', getcwd());
     // return sprintf('%s/%s', str_replace('/src', '', __DIR__), ltrim($str, '/'));
     $str = str_replace(['\\', '/./', '/../'], '/', trim($str));
     $str = preg_replace('|[^a-z0-9\*\-\_\./]|i', '', $str);
-    $path = sprintf('%s/%s', $root, ltrim($str, '/'));
+    $path = sprintf('%s/%s', PHICO_PATH_ROOT, ltrim($str, '/'));
 
     return $path;
 }
